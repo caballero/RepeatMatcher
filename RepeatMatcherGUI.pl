8a4ce59baeda47b9c12e8a41c6399ff1b78f63f7
Juan Caballero <jcaballero@systemsbiology.org>
2012-05-11 14:24:24 -0700

    devel

--- RepeatMatcherGUI.pl
+++ RepeatMatcherGUI.pl
@@ -79,8 +79,19 @@ my $reload   = undef;
 
 # Main variables
 my $our_version = 0.1;        # Script version number
+# %data => is the main struct
+#       $data{$id}
+#                  -> 'label'  : sequence fasta comment
+#                  -> 'seq'    : nucleotide sequence
+#                  -> 'self'   : sequence self-comparisons
+#                  -> 'align'  : sequence alignments to reference nucleotides
+#                  -> 'blastx' : sequence alignments to reference peptides
+#                  -> 'status' : sequence analysis status
 my %data;
-my ($cnt, $id);
+my $box_width  = 100;
+my $box_height = 40;
+my $call_id;
+my $delete;
 
 # Calling options
 GetOptions(
@@ -98,7 +109,6 @@ GetOptions(
 printVersion() if (defined $version);    
 pod2usage(-verbose => 2) if (defined $help);
 
-reloadProject() 
 if (defined $reload) {
     reloadProject($log);
 }
@@ -106,6 +116,69 @@ else {
     startLog($log);
 }
 
+# load data from files in %data
+loadIn();
+loadSelf();
+loadAlign();
+loadBlastx();
+my @ids = sort (keys %data);
+
+# Create the GUI
+my $mw = MainWindow -> new; # Main Window
+$mw -> title('RepeatMatcherGUI');
+
+
+# Edit frame
+my $edit_frame      = $mw -> Frame() -> pack(-side => 'top', -fill => 'x');
+my $label_entry     = $edit_frame -> Entry();
+my $update_button   = $edit_frame -> Button(-text => 'Update', -command => \&updateSeq);
+my $revcomp_button  = $edit_frame -> Button(-text => 'ReverseSeq', -command => \&reverseSeq);
+my $del_checkbox    = $edit_frame -> Checkbutton(-text => 'Delete', -variable => \$delete);
+
+# IDs list frame
+my $id_frame        = $mw -> Frame()-> pack(-side => 'left', -fill => 'y');
+
+foreach my $id (@ids) {
+    $id_frame -> Radiobutton( 
+                          -text       => $id, 
+                          -variable   => \$call_id, 
+                          -value      => $id, 
+                          -command    => \&callID,
+                          -width      => 20, 
+                          -height     => 2 * $box_height
+                         );
+}
+
+# Sequence frame
+my $seq_frame       = $mw -> Frame() -> pack(-side => 'top');
+my $seq_txt         = $seq_frame ->    Scrolled('Text', 
+                                                -scrollbars => "osoe", 
+                                                -width => $box_width, 
+                                                -height => $box_height);
+
+# Align frame
+my $align_frame     = $mw -> Frame() -> pack(-side => 'top');
+my $align_txt       = $align_frame ->  Scrolled('Text', 
+                                                -scrollbars => "osoe", 
+                                                -width => $box_width, 
+                                                -height => $box_height);
+
+# Self frame
+my $self_frame      = $mw -> Frame() -> pack(-side => 'bottom');
+my $self_txt        = $self_frame ->   Scrolled('Text', 
+                                                -scrollbars => "osoe", 
+                                                -width => $box_width, 
+                                                -height => $box_height);
+
+
+# Blastx frame
+my $blastx_frame    = $mw -> Frame() -> pack(-side => 'bottom');
+my $blastx_txt      = $blastx_frame -> Scrolled('Text', 
+                                                -scrollbars => "osoe", 
+                                                -width => $box_width, 
+                                                -height => $box_height);
+
+MainLoop();
 
 ###################################
 ####   S U B R O U T I N E S   ####
@@ -117,11 +190,40 @@ sub printVersion {
 }
 
 sub startLog {
-    
+    my $file = shift @_;
+    my $ver  = 0;
+    $ver = 1 if (defined $verbose); 
+    open LOG, ">$file" or die "cannot open $file\n";
+    print LOG <<_LOG_   
+# RepeatMatcherGUI log file
+seq_file: $in
+out_file: $out
+self_file: $self
+align_file: $align
+blastx_file: $blastx
+verbose_mode: $ver
+_LOG_
+;
 }
 
 sub reloadProject {
-    
+    my $file = shift @_;
+    open LOG, "+>$file" or die "cannot open $file\n";
+    while (<LOG>) {
+        chomp;
+        next if (m/^#/);
+        if    (m/^seq_file: (.+)/)     { $in      = $1; }
+        elsif (m/^out_file: (.+)/)     { $out     = $1; }
+        elsif (m/^self_file: (.+)/)    { $self    = $1; }
+        elsif (m/^align_file: (.+)/)   { $align   = $1; }
+        elsif (m/^blastx_file: (.+)/)  { $blastx  = $1; }
+        elsif (m/^verbose_mode: (.+)/) { $verbose = $1; }
+        else {
+            my ($id, $status) = split (/: /, $_);
+            $data{$id}{'status'} = $status;
+        }
+    }
+    $verbose = undef if ($verbose == 0);
 }
 
 sub loadIn {
@@ -131,7 +233,9 @@ sub loadIn {
     while (<F>) {
         if (m/>(.+?)#/) {
             $id = $1;
+            s/>//;
             $data{$id}{'label'} .= $_;
+            $data{$id}{'status'} = 'u' unless (defined $data{$id}{'status'});
         }
         else {
             $data{$id}{'seq'}   .= $_;
@@ -186,16 +290,48 @@ sub loadBlastx {
 }
 
 sub revcomp {
-    my $in  =  shift @_;
-    my @ln  =  split (/\n/, $in);
-    my $rc  =  shift @ln;
-       $rc .=  "\n";
-    my $sq  =  join "", @ln;
+    my $rc  =  '';
+    my $sq  =  shift @_;
+       $sq  =~ s/\n+//;
        $sq  =  reverse $sq;
        $sq  =~ tr/ACGTacgt/TGCAtgca/;
     while ($sq) {
-        $rc .= substr ($sq, 0, 50), "\n";
+        $rc .= substr ($sq, 0, 50);
+        $rc .= "\n";
         substr ($sq, 0, 50) = '';
     }
     return $rc;
+}
+
+sub callID {
+    my $lab_     = $call_id;
+    my $seq_     = 'No sequence';
+    my $self_    = 'No matches';
+    my $align_   = 'No matches';
+    my $blastx_  = 'No matches';
+    
+    $lab_        = $data{$call_id}{'label'}  if (defined $data{$call_id}{'label'}); 
+    $seq_        = $data{$call_id}{'seq'}    if (defined $data{$call_id}{'seq'});
+    $self_       = $data{$call_id}{'self'}   if (defined $data{$call_id}{'self'});
+    $align_      = $data{$call_id}{'align'}  if (defined $data{$call_id}{'align'});
+    $blastx_     = $data{$call_id}{'blastx'} if (defined $data{$call_id}{'blastx'});
+    
+    $seq_txt    -> delete();
+    $self_txt   -> delete();
+    $align_txt  -> delete();
+    $blastx_txt -> delete();
+    
+    $seq_txt    -> insert('end', ">$lab_\n$seq_");
+    $self_txt   -> insert('end', $self_);
+    $align_txt  -> insert('end', $align_);
+    $blastx_txt -> insert('end', $blastx_);
+}
+
+sub reverseSeq {
+    $data{$call_id}{'seq'} = revcomp($data{$call_id}{'seq'});
+    callID();
+}
+
+sub updateSeq {
+    
 }
