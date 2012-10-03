752918347d4e852c301c891f92be0f25b6de5979
Juan Caballero <jcaballero@systemsbiology.org>
2012-10-03 16:49:35 -0700

    debuggin

--- extendRepeatConsensus.pl
+++ extendRepeatConsensus.pl
@@ -245,7 +245,10 @@ sub extendRepeat {
         die "search engine $engine isn't supported\n"; 
     }
     
-    warn "$#left_seqs in left side, $#right_seqs in right side\n" if (defined $verbose); 
+    my $nleft  = scalar @left_seqs;
+    my $nright = scalar @right_seqs;
+    
+    warn "$nleft in left side, $nright in right side\n" if (defined $verbose); 
       
     if (($#left_seqs + 1)  >= $minseq) {
         $cons  = createConsensus("$ext$rep", @left_seqs);
